6b017bf194e97863f84013504bcfd5c0b4b37663
leoluo2017 <leoluo2017@gmail.com>
2020-02-25 10:59:33 -0500

    update

--- js/video.js
+++ js/video.js
@@ -1,52 +1,62 @@
 var video;
+var volume;
+var slider;
 
 function getVid(){
 	video = document.querySelector("#myVideo"); 
 }
 
 function playVid() { 
-
+	video.play();
 	console.log("Play Video");
+	console.log(video)
+	volume = document.querySelector("#volume")
+	console.log(volume);
+	volume.innerHTML=video.volume*100+"%";
 } 
 
 function pauseVid() { 
-
+	video.pause();
 	console.log("Pause Video");
 } 
 
 function decreaseSpeed() { 
-
-  	console.log("Speed is "+ );
+	video.playbackRate -=.1
+  	console.log("Speed is "+ video.playbackRate);
 } 
 
 function increaseSpeed() {
-
-	console.log("Speed is "+ );
+	video.playbackRate +=.1
+	console.log("Speed is "+ video.playbackRate);
 } 
 
-function skipAhead() {
+// function skipAhead() {
 
-	console.log("Current location is "+ );
-} 
+// 	console.log("Current location is "+ );
+// } 
 
-function mute() { 
+// function mute() { 
   	
-  		console.log("Unmuted");
+//   		console.log("Unmuted");
   	
-      	console.log("Muted");
-}
+//       	console.log("Muted");
+// }
 
 function changeVolume() {
-;	console.log("Volume is ");
+	let slider = document.querySelector("#volumeSlider")
+	console.log(slider.value)
+	video.volume=slider.value/100;
+	console.log("Volume is "+volume.volume/100);
+	volume.innerHTML=video.volume*100+"%";
 }
        
 
-function gray() { 
+// function gray() { 
 
-	console.log("In grayscale")
-}
+// 	console.log("In grayscale")
+// }
 
-function color() {
+// function color() {
 
-	console.log("In color") 
-}
+// 	console.log("In color") 
+// }
